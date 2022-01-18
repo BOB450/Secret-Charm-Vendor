4d4d83eb83cd17c2c2f4796ddd77d154cbdd64fa
BOB450 <bwvoskuil@gmail.com>
2022-01-17 19:50:11 -0700

    starting project files

---
+++ VanillaPlusPlusCharms/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Collections;
+using Modding;
+using ModCommon;
+using UnityEngine;
+using System.Security.Cryptography;
+using SFCore;
+using SFCore.Generics;
+
+namespace VanillaPlusPlusCharms
+{
+    public class Class2 : Mod
+    {
+        public Class2() : base("VanillaPlusPlusCharms") { }
+       // public override string GetVersion() => "0.1";
+        public override void Initialize()
+        {
+            Log("Inits");
+
+
+            ModHooks.AfterTakeDamageHook += AfterTakeDamage;
+
+
+        }
+
+        private int AfterTakeDamage(int hazardType, int damageAmount)
+        {
+            PlayerData pd = PlayerData.instance;
+            
+            return damageAmount;
+        }
+      
+
+
+
+    }
+}
